f560c34637dd19e4ee0e238e1084a33739428264
Ariel Marcelo Pardo <adoorno@gmail.com>
2026-01-10 22:34:59 -0300

    Change upload endpoint in order to download file. Add .gitignore.

--- src/fastapiprob/models.py
+++ src/fastapiprob/models.py
@@ -5,20 +5,17 @@ class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
     role: str
-    email: str
+    email: str = Field(index=True, unique=True)
     hash_pass: str
     
-class PdfInvoice(SQLModel, table=True):
+class Document(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     user_id: int = Field(foreign_key="user.id")
-    name: str
-    created_at: datetime=Field(default_factory=datetime.utcnow)
 
-class ExcelFromInvoice(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    user_id: int = Field(foreign_key="user.id")
-    name: str
-    pdf_id: int= Field(foreign_key="pdfinvoice.id", unique=True)
-    created_at: datetime=Field(default_factory=datetime.utcnow)
-    
-        
+    pdf_name: str
+    pdf_path: str
+
+    excel_name: str
+    excel_path: str
+
+    created_at: datetime = Field(default_factory=datetime.utcnow)
--- src/fastapiprob/routes/documents.py
+++ src/fastapiprob/routes/documents.py
@@ -1,12 +1,16 @@
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, UploadFile, Depends
 from pathlib import Path
 import shutil
 from fastapiprob.pdf_service import convert_pdf_to_excel
+from fastapi.responses import FileResponse
+from sqlmodel import Session
+from fastapiprob.database import get_session
+from fastapiprob.models import Document
 
 router = APIRouter()
 
 @router.post("/documents/upload")
-async def upload_file(file: UploadFile):
+async def upload_file(file: UploadFile, session: Session = Depends(get_session)):
     pdf_path = Path("data/pdf") / file.filename #operator "/" overloaded by pathlib.Path
     excel_path = Path("data/excel") / (pdf_path.stem + ".xlsx")
     pdf_path.parent.mkdir(parents=True, exist_ok=True)
@@ -18,7 +22,19 @@ async def upload_file(file: UploadFile):
     file.file.close()
     convert_pdf_to_excel(pdf_path, excel_path)
 
-    return {
-        "pdf": str(pdf_path),
-        "excel": str(excel_path),
-    }    
+    document = Document(
+        user_id=1,  #temporary
+        pdf_name=file.filename,
+        pdf_path=str(pdf_path),
+        excel_name=excel_path.name,
+        excel_path=str(excel_path),
+    )
+
+    session.add(document)
+    session.commit()
+
+    return FileResponse(
+        path=excel_path,
+        filename=excel_path.name,
+        media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
